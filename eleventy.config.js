47a460750667b9dd38497997afa8f6a6f85124a0
Hatsumi <k1rishima@proton.me>
2026-03-28 19:16:56 +0800

    feat: RSS feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,26 +1,30 @@
-import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
+import {
+	IdAttributePlugin,
+	InputPathToUrlTransformPlugin,
+	HtmlBasePlugin,
+} from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import yaml from "js-yaml";
 import pluginFilters from "./_config/filters.js";
-import { execSync } from 'child_process';
-import markdownIt from 'markdown-it';
+import { execSync } from "child_process";
+import markdownIt from "markdown-it";
 import markdownItAnchor from "markdown-it-anchor";
 import markdownItFootnote from "markdown-it-footnote";
-import markdownItAttrs from 'markdown-it-attrs';
-import pluginTOC from 'eleventy-plugin-toc';
+import markdownItAttrs from "markdown-it-attrs";
+import pluginTOC from "eleventy-plugin-toc";
 import CleanCSS from "clean-css";
 
-export default async function(eleventyConfig) {
+export default async function (eleventyConfig) {
 	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+		if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
 			return false;
 		}
 	});
 	eleventyConfig
 		.addPassthroughCopy({
-			"./public/": "/"
+			"./public/": "/",
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 	eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpeg}");
@@ -33,27 +37,31 @@ export default async function(eleventyConfig) {
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
 	});
-	
-eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		preAttributes: { tabindex: 0 }
-	});  let options = {
-    html: true,
-    breaks: true,
-    linkify: true,
-      permalink: true,
-    typographer: true,
-      permalinkClass: "direct-link",
-      permalinkSymbol: "#"
-  };
-   let markdownLib = markdownIt(options).use(markdownItAttrs).use(markdownItFootnote);
-  eleventyConfig.setLibrary("md", markdownLib);
 
+	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+		preAttributes: { tabindex: 0 },
+	});
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+		permalink: true,
+		typographer: true,
+		permalinkClass: "direct-link",
+		permalinkSymbol: "#",
+	};
+	let markdownLib = markdownIt(options)
+		.use(markdownItAttrs)
+		.use(markdownItFootnote);
+	eleventyConfig.setLibrary("md", markdownLib);
 
-  eleventyConfig.on('eleventy.after', () => {
-		execSync(`npx pagefind --site _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
-	  })
+	eleventyConfig.on("eleventy.after", () => {
+		execSync(`npx pagefind --site _site --glob \"**/*.html\"`, {
+			encoding: "utf-8",
+		});
+	});
 
-	  eleventyConfig.amendLibrary("md", mdLib => {
+	eleventyConfig.amendLibrary("md", (mdLib) => {
 		mdLib.use(markdownItAnchor, {
 			permalink: markdownItAnchor.permalink.ariaHidden({
 				placement: "after",
@@ -61,25 +69,24 @@ eleventyConfig.addPlugin(pluginSyntaxHighlight, {
 				symbol: "",
 				ariaHidden: false,
 			}),
-			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
+			level: [1, 2, 3, 4],
+			slugify: eleventyConfig.getFilter("slugify"),
 		});
 	});
 
-	  eleventyConfig.addPlugin(pluginTOC, {
-		tags: ['h2', 'h3', 'h4', 'h5'],
-		  id: 'toci', 
-		  class: 'list-group',
+	eleventyConfig.addPlugin(pluginTOC, {
+		tags: ["h2", "h3", "h4", "h5"],
+		id: "toci",
+		class: "list-group",
 		ul: true,
 		flat: true,
-		wrapper: 'div'
-	  });
-
+		wrapper: "div",
+	});
 
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(HtmlBasePlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
-    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 	eleventyConfig.addPlugin(feedPlugin, {
 		type: "atom", // or "rss", "json"
 		outputPath: "/feed/feed.xml",
@@ -87,8 +94,8 @@ eleventyConfig.addPlugin(pluginSyntaxHighlight, {
 		templateData: {
 			eleventyNavigation: {
 				key: "Feed",
-				order: 4
-			}
+				order: 4,
+			},
 		},
 		collection: {
 			name: "posts",
@@ -98,36 +105,29 @@ eleventyConfig.addPlugin(pluginSyntaxHighlight, {
 			language: "en",
 			title: "Blog Title",
 			subtitle: "This is a longer description about your blog.",
-			base: "https://example.com/",
+			base: "https://0t63.github.io/",
 			author: {
-				name: "Your Name"
-			}
-		}
+				name: "Your Name",
+			},
+		},
 	});
 
 	eleventyConfig.addPlugin(pluginFilters);
-	eleventyConfig.addPlugin(IdAttributePlugin, {
-	});
+	eleventyConfig.addPlugin(IdAttributePlugin, {});
 
 	eleventyConfig.addShortcode("currentBuildDate", () => {
-		return (new Date()).toISOString();
+		return new Date().toISOString();
 	});
-};
-	
+}
+
 export const config = {
-	templateFormats: [
-		"md",
-		"njk",
-		"html",
-		"liquid",
-		"11ty.js",
-	],
+	templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
 	markdownTemplateEngine: "njk",
 	htmlTemplateEngine: "njk",
 	dir: {
 		input: "content",
 		includes: "../_includes",
 		data: "../_data",
-		output: "_site"
+		output: "_site",
 	},
 };
